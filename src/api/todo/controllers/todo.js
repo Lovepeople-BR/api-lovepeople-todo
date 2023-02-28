1dcf22021300ee6b4416e537de264cf4ede1883e
Rafael Barbosa <rafael@usedrip.com.br>
2023-02-28 11:40:34 -0300

    update controller

--- src/api/todo/controllers/todo.js
+++ src/api/todo/controllers/todo.js
@@ -6,4 +6,36 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::todo.todo');
+module.exports = createCoreController('api::todo.todo', ({strapi}) => ({
+    async find(ctx) {
+        const user = ctx.state.user
+        if (!user) {
+        return ctx.badRequest(null, [{messages: [{id: "No auth header found"}]}])
+        }
+
+        const data = await strapi.entityService.findMany("api::todo.todo", {
+        populate: 'image',
+        filters: {
+            "user": {
+                "id": user.id
+            }
+        },
+
+        });
+        if (!data) {
+        return ctx.notFound();
+        }
+
+        const sanitizedEvents = await this.sanitizeOutput(data, ctx);
+
+        return this.transformResponse(sanitizedEvents);
+    },
+    async create(ctx) {
+      const {id} = ctx.state.user; //ctx.state.user contains the current authenticated user
+      const response = await super.create(ctx);
+      const updatedResponse = await strapi.entityService
+        .update('api::todo.todo', response.data.id, {data: {user: id}})
+      return updatedResponse;
+    },
+    
+  }));
